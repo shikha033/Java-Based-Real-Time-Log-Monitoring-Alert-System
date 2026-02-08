70d6bda67c217efde9016cf83de56aa41a0db3f7
shikha033 <shikhasingh242494@gmail.com>
2026-02-08 23:03:35 +0530

    Update LogEntry.java

--- src/main/java/com/logmonitor/model/LogEntry.java
+++ src/main/java/com/logmonitor/model/LogEntry.java
@@ -1,7 +1,18 @@
+
 package com.logmonitor.model;
+
 import java.time.LocalDateTime;
+
 public class LogEntry {
     private LocalDateTime timestamp;
     private String level;    // INFO, WARN, ERROR
     private String message;
     private String source;
+
+    // Constructor
+    public LogEntry(LocalDateTime timestamp, String level, String message, String source) {
+        this.timestamp = timestamp;
+        this.level = level;
+        this.message = message;
+        this.source = source;
+    }
