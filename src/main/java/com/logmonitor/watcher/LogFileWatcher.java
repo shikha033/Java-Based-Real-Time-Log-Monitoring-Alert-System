724e7898a38035b99fab3d701098bdfb5a517645
shikha033 <shikhasingh242494@gmail.com>
2026-02-21 00:46:01 +0530

    Update LogFileWatcher.java

--- src/main/java/com/logmonitor/watcher/LogFileWatcher.java
+++ src/main/java/com/logmonitor/watcher/LogFileWatcher.java
@@ -1,8 +1,6 @@
 package com.logmonitor.watcher;
 
 import com.logmonitor.model.LogEntry;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
