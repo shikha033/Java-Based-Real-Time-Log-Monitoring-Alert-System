4f951b03d918dff8368bfdae3975b39745bcb192
shikha033 <shikhasingh242494@gmail.com>
2026-02-28 21:37:23 +0530

    Create Alert.java

---
+++ src/main/java/com/logmonitor/alert/Alert.java
@@ -0,0 +1,10 @@
+package com.logmonitor.alert;
+
+import java.time.LocalDateTime;
+
+public class Alert {
+
+    private String type;
+    private String message;
+    private LocalDateTime triggeredAt;
+    private String severity;
