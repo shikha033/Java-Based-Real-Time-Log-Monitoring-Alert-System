83ac93546f19940425d1e008ab9193b1b6b38166
shikha033 <shikhasingh242494@gmail.com>
2026-03-23 22:41:30 +0530

    Create DatabaseService.java

---
+++ src/main/java/com/logmonitor/db/DatabaseService.java
@@ -0,0 +1,18 @@
+package com.logmonitor.db;
+import com.logmonitor.model.LogEntry;
+import com.logmonitor.alert.Alert;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseService {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/logmonitor";
+    private static final String USER = "root";
+    private static final String PASSWORD = "yourpassword";
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
