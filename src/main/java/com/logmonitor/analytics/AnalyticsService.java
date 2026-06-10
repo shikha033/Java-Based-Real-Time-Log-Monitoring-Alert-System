7850989140d9136082a595fa972963374d8415eb
shikha033 <shikhasingh242494@gmail.com>
2026-06-10 18:18:32 +0530

    Implemented AnalyticsService for database statistics

--- src/main/java/com/logmonitor/analytics/AnalyticsService.java
+++ src/main/java/com/logmonitor/analytics/AnalyticsService.java
@@ -5,3 +5,32 @@ import com.logmonitor.db.DatabaseService;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+public class AnalyticsService {
+
+    private DatabaseService dbService = new DatabaseService();
+
+    public void showStatistics() {
+
+        try (Connection conn = dbService.connect();
+             Statement stmt = conn.createStatement()) {
+
+            ResultSet rs1 = stmt.executeQuery(
+                    "SELECT COUNT(*) FROM logs");
+
+            if (rs1.next()) {
+                System.out.println("Total Logs: " + rs1.getInt(1));
+            }
+
+            ResultSet rs2 = stmt.executeQuery(
+                    "SELECT COUNT(*) FROM alerts");
+
+            if (rs2.next()) {
+                System.out.println("Total Alerts: " + rs2.getInt(1));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
