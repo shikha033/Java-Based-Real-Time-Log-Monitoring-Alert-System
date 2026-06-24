4a7b8afc0d963be8834af05643b15dc6eab4556c
shikha033 <shikhasingh242494@gmail.com>
2026-06-24 23:30:37 +0530

    Implemented properties file reader using ConfigReader

--- src/main/java/com/logmonitor/config/ConfigReader.java
+++ src/main/java/com/logmonitor/config/ConfigReader.java
@@ -3,3 +3,22 @@ package com.logmonitor.config;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties properties = new Properties();
+
+    public ConfigReader() {
+
+        try {
+
+            FileInputStream file =
+                    new FileInputStream("config/application.properties");
+
+            properties.load(file);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
