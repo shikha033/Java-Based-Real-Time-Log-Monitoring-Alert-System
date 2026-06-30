e87e5bebb6ec42db84fcd92d02dea9a58d9a93bb
shikha033 <shikhasingh242494@gmail.com>
2026-06-30 22:50:48 +0530

    Added log search service

--- src/main/java/com/logmonitor/search/LogSearchService.java
+++ src/main/java/com/logmonitor/search/LogSearchService.java
@@ -5,3 +5,19 @@ import com.logmonitor.db.DatabaseService;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+
+public class LogSearchService {
+
+
+    private DatabaseService dbService =
+            new DatabaseService();
+
+
+    public void searchByLevel(String level) {
+
+
+        String sql =
+        "SELECT * FROM logs WHERE level=?";
+
+
